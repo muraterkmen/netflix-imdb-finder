8170db583ec122b8274207380c77a375a06c4893
Murat ERKMEN <mrterkmn@gmail.com>
2021-11-07 21:00:16 +0300

    content script update

--- contentScript.js
+++ contentScript.js
@@ -1,6 +1,19 @@
 // document.body.style.backgroundColor = "orange";
 
-var theDiv = document.getElementsByClassName("nfInputPlacement")[0];
-var newNode = document.createElement("p");
+const theDiv = document.getElementsByClassName("nfInputPlacement")[0];
+const newNode = document.createElement("p");
 newNode.innerHTML = "burası muştur yolu yokuştur";
 theDiv.appendChild(newNode);
+
+
+const firstFilm = document.querySelectorAll("div.ptrack-content a[class='slider-refocus']")[0];
+
+// if (!films && films.isArrays()) {
+//     films.forEach( function(fil) {
+//         console.log(fil.getAttribute("aria-label"));
+//     });    
+// }
+
+const filmName = firstFilm.getAttribute("aria-label");
+
+console.log(filmName);
