02785bed0d02ce2f66e39f418995b8c4821b7f7c
Murat ERKMEN <mrterkmn@gmail.com>
2021-11-07 16:52:41 +0300

    content script activeted

--- background.js
+++ background.js
@@ -1,91 +1,112 @@
-let color = "#3aa757";
+// let color = "#3aa757";
 
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log("Default background color set to %cgreen", `color: ${color}`);
+// chrome.runtime.onInstalled.addListener(() => {
+//   chrome.storage.sync.set({ color });
+//   console.log("Default background color set to %cgreen", `color: ${color}`);
 
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    // since only one tab should be active and in the current window at once
-    // the return variable should only have one entry
-    var activeTab = tabs[0];
-    var activeTabId = activeTab.id; // or do whatever you need
-  });
+//   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+//     // since only one tab should be active and in the current window at once
+//     // the return variable should only have one entry
+//     var activeTab = tabs[0];
+//     var activeTabId = activeTab.id; // or do whatever you need
+//   });
 
-//  chrome.browserAction.onClicked.addListener(function (tab) {
-//    var found = false;
-//    var tabId;
-//    chrome.tabs.query({}, function (tabs) {
-//      for (var i = 0; i < tabs.length; i++) {
-//        if (tabs[i].url.search("www.pandora.com/") > -1) {
-//          found = true;
-//          tabId = tabs[i].id;
-//        }
-//      }
-//      if (found == false) {
-//        chrome.tabs.executeScript(null, { file: "buy.js" });
-//      } else {
-//        chrome.tabs.update(tabId, { selected: true });
-//      }
-//    });
-//  });
+  
+// //  chrome.browserAction.onClicked.addListener(function (tab) {
+// //    var found = false;
+// //    var tabId;
+// //    chrome.tabs.query({}, function (tabs) {
+// //      for (var i = 0; i < tabs.length; i++) {
+// //        if (tabs[i].url.search("www.pandora.com/") > -1) {
+// //          found = true;
+// //          tabId = tabs[i].id;
+// //        }
+// //      }
+// //      if (found == false) {
+// //        chrome.tabs.executeScript(null, { file: "buy.js" });
+// //      } else {
+// //        chrome.tabs.update(tabId, { selected: true });
+// //      }
+// //    });
+// //  });
 
-  chrome.tabs.onCreated.addListener(function (activeInfo, second) {
-    console.log("onCreated");
+//   chrome.tabs.onCreated.addListener(function (activeInfo, second) {
+//     console.log("onCreated");
 
-    chrome.tabs.getSelected(null, function (tab) {
-      if (tab && tab.url) {
-        console.log("onCreated tab url:" + tab.url);
-      }
-    });
-  });
+//     chrome.tabs.getSelected(null, function (tab) {
+//       if (tab && tab.url) {
+//         console.log("onCreated tab url:" + tab.url);
+//       }
+//     });
+//   });
 
-  chrome.tabs.onUpdated.addListener(function (activeInfo, second) {
-    console.log("onUpdated");
-    chrome.tabs.getSelected(null, function (tab) {
-      if (tab && tab.url) {
-        console.log("onUpdated tab url:" + tab.url);
-      }
-    });
-  });
+//   chrome.tabs.onUpdated.addListener(function (activeInfo, second) {
+//     console.log("onUpdated");
+//     chrome.tabs.getSelected(null, function (tab) {
+//       if (tab && tab.url) {
+//         console.log("onUpdated tab url:" + tab.url);
+//       }
+//     });
+//   });
 
-//  chrome.tabs.onActiveChanged.addListener(function (activeInfo, second) {
-//    console.log("onActiveChanged");
-//    chrome.tabs.getSelected(null, function (tab) {
-//      if (tab && tab.url) {
-//        console.log("onActiveChanged tab url:" + tab.url);
-//      }
-//    });
-//  });
+// //  chrome.tabs.onActiveChanged.addListener(function (activeInfo, second) {
+// //    console.log("onActiveChanged");
+// //    chrome.tabs.getSelected(null, function (tab) {
+// //      if (tab && tab.url) {
+// //        console.log("onActiveChanged tab url:" + tab.url);
+// //      }
+// //    });
+// //  });
 
-  chrome.tabs.onActivated.addListener(function (activeInfo, second) {
-    console.log("onActivated" + activeInfo.tabId);
-    //chrome.tabs.query({}, function (tabs) {
-    //  for (var i = 0; i < tabs.length; i++) {
-    //    if (tabs[i].id == activeInfo.tabId)
-    //      console.log("onActiveChanged tab url:" + tabs[i].url);
-    //  }
-    //});
+//   chrome.tabs.onActivated.addListener(function (activeInfo, second) {
+//     console.log("onActivated" + activeInfo.tabId);
+//     //chrome.tabs.query({}, function (tabs) {
+//     //  for (var i = 0; i < tabs.length; i++) {
+//     //    if (tabs[i].id == activeInfo.tabId)
+//     //      console.log("onActiveChanged tab url:" + tabs[i].url);
+//     //  }
+//     //});
 
 
-    chrome.tabs.get(activeInfo.tabId, async (tab) => {
-        //let muted = !tab.mutedInfo.muted;
-        //await chrome.tabs.update(tabId, { muted });
+//     chrome.tabs.get(activeInfo.tabId, async (tab) => {
+//         //let muted = !tab.mutedInfo.muted;
+//         //await chrome.tabs.update(tabId, { muted });
 
-        const isNetflix=tab.url.search('https://www.netflix.com/')
+//         const isNetflix=tab.url.search('https://www.netflix.com/')
 
-        if(tab.url.search('https://www.netflix.com/')!==-1){
-            console.log("netflix found");
+//         function changeBackgroundColor() {
+//           // document.body.style.backgroundColor = "orange";
 
-        }
-        console.log("onActiveChanged tab url:" + tab.url);
-      });
-  });
+//           // var theDiv = document.getElementsByClassName("nfInputPlacement")[0];
+//           // var newNode = document.createElement('p');
+//           //  newNode.innerHTML = 'burası muştur';
+//           // theDiv.appendChild(newNode);
 
-  //chrome.action.onClicked.addListener(function(tab) {
-  //    // No tabs or host permissions needed!
-  //    console.log('Turning ' + tab.url + ' red!');
-  //    chrome.scripting.executeScript({
-  //      code: 'document.body.style.backgroundColor="red"'
-  //    });
-  //  });
-});
+//         }
+
+//         const tabId = activeInfo.tabId;
+
+//         if(tab.url.search('https://www.netflix.com/')!==-1){
+//             console.log("netflix found");
+            
+//             chrome.scripting.executeScript(
+//               {
+//                 target: {tabId: tabId},
+//                 func: changeBackgroundColor,
+//               },
+//               (injectionResults) => {
+//                 console.log('ExeceuteScript get error: ' + injectionResults);
+//               });
+//         }
+//         console.log("onActiveChanged tab url:" + tab.url);
+//       });
+//   });
+
+//   //chrome.action.onClicked.addListener(function(tab) {
+//   //    // No tabs or host permissions needed!
+//   //    console.log('Turning ' + tab.url + ' red!');
+//   //    chrome.scripting.executeScript({
+//   //      code: 'document.body.style.backgroundColor="red"'
+//   //    });
+//   //  });
+// });
--- contentScript.js
+++ contentScript.js
@@ -1,2 +1,6 @@
+// document.body.style.backgroundColor = "orange";
 
-document.body.style.backgroundColor = 'orange';
+var theDiv = document.getElementsByClassName("nfInputPlacement")[0];
+var newNode = document.createElement("p");
+newNode.innerHTML = "burası muştur yolu yokuştur";
+theDiv.appendChild(newNode);
